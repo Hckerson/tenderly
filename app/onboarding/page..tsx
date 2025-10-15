8772c6558e7ee74d057b41e9d9dbc44ce14e698f
Hckerson <166761968+Hckerson@users.noreply.github.com>
2025-10-15 23:15:13 +0100

    update: complete build process

---
+++ app/onboarding/page..tsx
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+import { SwiperFlatList } from 'react-native-swiper-flatlist';
+
+export default function OnboardingPageOne() {
+    return(
+        <View className="px-5">
+
+        </View>
+    )
+}
---
+++ lib/place-holder.tsx
@@ -0,0 +1,21 @@
+const onboardingData = [
+	{
+		id: 1,
+		title: "Support for Every Pregnant Moment",
+		about: "Track your pregnancy, get expert tips, and feel supported through every trimester.",
+		action: "Next",
+	},
+	{
+		id: 2,
+		title: "Speak Directly with Healthcare doctors",
+		about: "Instantly connect via secure chat or video for expert medical advice anytime.",
+		action: "Next",
+	},
+
+	{
+		id: 3,
+		title: "book consultations your favorite doctor",
+		about: "book an appointment with your favorite doctor and get the better solution for you.",
+		action: "get started",
+	},
+];
