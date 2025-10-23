02661b45ebb92bb9bc6aa0f70e118547f4aa71bb
Hckerson <166761968+Hckerson@users.noreply.github.com>
2025-10-23 18:29:06 +0100

    patch: added safeareview to root layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,8 @@
 import { Stack } from "expo-router";
 import { createContext } from "react";
 import { Dimensions } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
 import "../global.css";
 
 const { width } = Dimensions.get("window");
@@ -10,7 +12,9 @@ export const WidthContext = createContext(width);
 export default function RootLayout() {
     return (
         <WidthContext value={width}>
-            <Stack />
+            <SafeAreaProvider>
+                <Stack />
+            </SafeAreaProvider>
         </WidthContext>
     );
 }
