5cf61600f715124800395319300b9058d3639320
Hckerson <166761968+Hckerson@users.noreply.github.com>
2025-10-30 13:39:08 +0100

    update: complete forget password page

--- app/(auth)/reset-password/index.tsx
+++ app/(auth)/reset-password/index.tsx
@@ -1,15 +1,14 @@
 import { WidthContext } from "@/app/_layout";
 import Back from "@/components/ui/back";
 import Button from "@/components/ui/button";
-import { useContext } from "react";
+import Feather from "@expo/vector-icons/Feather";
+import { useContext, useState } from "react";
 import { Platform, Text, TextInput, View } from "react-native";
-
-const inbox = require("@/assets/svgs/inbox.svg");
 
 export default function PasswordReset() {
     const width = useContext(WidthContext);
     const native = Platform.OS === "android" || Platform.OS === "ios";
-
+    const [hiddenStatus, setHiddenStatus] = useState<boolean>(true);
     return (
         <View
             style={{ width }}
@@ -19,38 +18,82 @@ export default function PasswordReset() {
                 <Back />
                 <View className="flex">
                     <Text className="w-3/4 text-2xl font-semibold  text-text-primary">
-                        Use phone or email to reset your password.
+                        Secure your account with new passwords
                     </Text>
                     <Text className="text-sm font-normal text-text-secondary-dark">
-                        Enter your email or phone to reset your password easily.
+                        Create a strong password to secure your account now.
                     </Text>
                 </View>
             </View>
             <View className="h-full gap-y-8 rounded-t-[30px] bg-background-tertiary     p-5 dark:bg-background-tertiary-dark">
                 <View className="grid gap-y-6">
                     <View className="flex flex-col gap-y-2">
-                        <Text className="text-text-secondary">Email</Text>
-                        <TextInput
-                            className="border-1 h-12 rounded-[10px] border-[#EFEFEF] bg-background-pent pl-4 placeholder:text-text-primary-dark dark:border-background-quad-dark dark:bg-background-quad-dark placeholder:dark:text-text-primary"
+                        <Text className="text-text-secondary">Password</Text>
+                        <View
+                            className="border-1  relative flex h-12 justify-center rounded-[10px] border-[#EFEFEF] bg-background-pent placeholder:text-text-primary-dark dark:border-background-quad-dark dark:bg-background-quad-dark placeholder:dark:text-text-primary "
                             style={{ borderWidth: 1 }}
-                            placeholder="wadewarren@gmail.com"
-                            textContentType="emailAddress"
-                            inputMode="text"
-                        />
+                        >
+                            <TextInput
+                                className="h-full rounded-[10px] placeholder:text-[14px] border-[#EFEFEF] bg-background-pent pl-4 placeholder:text-text-primary-dark dark:border-background-quad-dark dark:bg-background-quad-dark placeholder:dark:text-text-primary"
+                                placeholder="Enter your new password"
+                                inputMode="text"
+                                textContentType="password"
+                                secureTextEntry={hiddenStatus}
+                            />
+                            <View className="absolute right-1 z-10 flex w-[40px] items-center">
+                                {hiddenStatus ? (
+                                    <Feather
+                                        name="eye-off"
+                                        size={18}
+                                        color="black"
+                                        onPress={() => setHiddenStatus(false)}
+                                    />
+                                ) : (
+                                    <Feather
+                                        name="eye"
+                                        size={18}
+                                        color="black"
+                                        onPress={() => setHiddenStatus(true)}
+                                    />
+                                )}
+                            </View>
+                        </View>
                     </View>
                     <View className="flex flex-col gap-y-2">
-                        <Text className="text-text-secondary">Phone</Text>
-                        <TextInput
-                            className="border-1 h-12 rounded-[10px] border-[#EFEFEF] bg-background-pent pl-4 placeholder:text-text-primary-dark dark:border-background-quad-dark dark:bg-background-quad-dark placeholder:dark:text-text-primary"
+                        <Text className="text-text-secondary">Confirm password</Text>
+                        <View
+                            className="border-1  relative flex h-12 justify-center rounded-[10px] border-[#EFEFEF] bg-background-pent placeholder:text-text-primary-dark dark:border-background-quad-dark dark:bg-background-quad-dark placeholder:dark:text-text-primary "
                             style={{ borderWidth: 1 }}
-                            placeholder="Enter your phone number"
-                            textContentType="telephoneNumber"
-                            inputMode="text"
-                        />
+                        >
+                            <TextInput
+                                className="h-full rounded-[10px] border-[#EFEFEF] bg-background-pent pl-4 placeholder:text-text-primary-dark placeholder:text-[14px] dark:border-background-quad-dark dark:bg-background-quad-dark placeholder:dark:text-text-primary"
+                                placeholder="Confirm your new password"
+                                inputMode="text"
+                                textContentType="password"
+                                secureTextEntry={hiddenStatus}
+                            />
+                            <View className="absolute right-1 z-10 flex w-[40px] items-center">
+                                {hiddenStatus ? (
+                                    <Feather
+                                        name="eye-off"
+                                        size={18}
+                                        color="black"
+                                        onPress={() => setHiddenStatus(false)}
+                                    />
+                                ) : (
+                                    <Feather
+                                        name="eye"
+                                        size={18}
+                                        color="black"
+                                        onPress={() => setHiddenStatus(true)}
+                                    />
+                                )}
+                            </View>
+                        </View>
                     </View>
                 </View>
 
-                <Button size="lg">Continue</Button>
+                <Button size="lg">Reset Password</Button>
             </View>
         </View>
     );
